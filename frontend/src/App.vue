a2cc3f17b72039f7757b3265d3a499aceb4b59c2
Alina Konkova <99170615+AlinaKonkova@users.noreply.github.com>
2024-09-26 23:43:45 +0300

    fr

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Лабораторная работа №1</h1>
+    <input v-model="inputValue" placeholder="Введите данные" />
+    <button @click="sendData">Отправить</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: ''
+    };
+  },
+  methods: {
+    async sendData() {
+      try {
+        const response = await fetch('http://localhost:8000/api/data/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ data: this.inputValue }),
+        });
+        if (response.ok) {
+          console.log('Данные успешно отправлены');
+        }
+      } catch (error) {
+        console.error('Ошибка отправки данных', error);
+      }
+    },
+  },
+};
+</script>
